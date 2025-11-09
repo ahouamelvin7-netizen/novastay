491f45462bf87c9b083a04fbf9ac2c417740dda0
ahouamelvin7-netizen <ahouamelvin7@gmail.com>
2025-11-09 14:21:30 -0400

    Create page.js cancel

---
+++ app/cancel/page.js
@@ -0,0 +1,8 @@
+export default function Cancel() {
+  return (
+    <main style={{minHeight:"60vh",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
+      <h1 style={{color:"#f59e0b"}}>Paiement annulé</h1>
+      <a href="/" style={{marginTop:20,color:"#f59e0b"}}>Réessayer</a>
+    </main>
+  );
+}
