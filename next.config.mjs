9715379898693006cbf95c1a34ec7bc6f8ccab77
ahouamelvin7-netizen <ahouamelvin7@gmail.com>
2025-11-11 19:22:10 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/success',
-        destination: '/app/success',
-        permanent: true,
-      },
-    ];
-  },
-};
 
-export default nextConfig;
