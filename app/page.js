6ccfd292ccc2d5ea12b380c25a8da5dad0b1d1a3
ahouamelvin7-netizen <ahouamelvin7@gmail.com>
2025-11-11 19:40:38 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+export default function HomePage() {
+  return (
+    <main style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      background: 'linear-gradient(to bottom, #111, #333)',
+      color: 'white',
+      textAlign: 'center',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1>Votre villa, votre paradis</h1>
+      <p>NovaStay – Luxe moderne & chaleur caribéenne</p>
+      <p>Tarif demo : 450 € / nuit</p>
+    </main>
+  )
+}
